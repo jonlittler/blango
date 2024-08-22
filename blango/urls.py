7c3ef1eb13b14d43c34a4f8c8bb7bca697ef1449
Jon Littler <jonathan.littler@ubs.com>
2024-08-22 01:14:37 +0000

    Finish First-Party Django REST API

--- blango/urls.py
+++ blango/urls.py
@@ -30,19 +30,22 @@ from blango_auth.forms import BlangoRegistrationForm
 # print(f"Time zone: {settings.TIME_ZONE}")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', blog.views.index),
-    path('post/<slug>/', blog.views.post_detail, name='blog-post-detail'),
-    path("ip/", blog.views.get_ip, name='ip'),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path("accounts/profile/", blango_auth.views.profile, name="profile"),
+  path('admin/', admin.site.urls),
+  path('', blog.views.index),
+  path('post/<slug>/', blog.views.post_detail, name='blog-post-detail'),
+  path("ip/", blog.views.get_ip, name='ip'),
+  path('accounts/', include('django.contrib.auth.urls')),
+  path("accounts/profile/", blango_auth.views.profile, name="profile"),
 
-    # django registration two step
-    path("accounts/register/", RegistrationView.as_view(form_class=BlangoRegistrationForm), name="django_registration_register"),
-    path('accounts/', include('django_registration.backends.activation.urls')),
+  # django registration two step
+  path("accounts/register/", RegistrationView.as_view(form_class=BlangoRegistrationForm), name="django_registration_register"),
+  path('accounts/', include('django_registration.backends.activation.urls')),
+
+  # rest api
+  path("api/v1/", include("blog.api_urls")),
 ]
 
 if settings.DEBUG:
-    urlpatterns += [
-        path("__debug__/", include(debug_toolbar.urls)),
-    ]
+  urlpatterns += [
+    path("__debug__/", include(debug_toolbar.urls)),
+  ]
